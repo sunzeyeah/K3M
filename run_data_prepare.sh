404eea4392d72dfa8ef1eb5fda9853e43d363160
sunzeyeah <sunzeyeah@sina.com>
2022-06-25 12:07:59 +0800

    update run_data_prepare.sh

--- run_data_prepare.sh
+++ run_data_prepare.sh
@@ -7,7 +7,7 @@
 # data processing
 ROOT_DIR="/root/autodl-tmp/Data/ccks2022/task9"
 DATA_DIR=${ROOT_DIR}/raw
-OUTPUT_DIR=${ROOT_DIR}/processed
+OUTPUT_DIR=${ROOT_DIR}/processed/k3m/
 CV_MODEL_CONFIG="/root/autodl-tmp/Data/ccks2022/task9/output/cv_model/VG-Detection/faster_rcnn_R_101_C4_caffe.yaml"
 CV_MODEL_FILE="/root/autodl-tmp/Data/ccks2022/task9/output/cv_model/faster_rcnn_from_caffe.pkl"
 MAIN="/root/Code/K3M/data_prepare.py"
@@ -15,8 +15,8 @@ MAIN="/root/Code/K3M/data_prepare.py"
 python $MAIN \
   --data_dir $DATA_DIR \
   --output_dir $OUTPUT_DIR \
-  --file_item_info "item_{}_info.jsonl" \
-  --file_image "item_{}_images" \
+  --file_item_info "item_info.jsonl" \
+  --file_image "item_images" \
   --cv_model_config $CV_MODEL_CONFIG \
   --cv_model_file $CV_MODEL_FILE \
   --is_cuda
